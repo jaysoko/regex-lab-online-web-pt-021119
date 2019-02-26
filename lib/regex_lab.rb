476c6b9364a7f1251179a265645edcceaaf813fe
Jay Sokolowski <jaysoko@gmail.com>
2019-02-26 02:42:31 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,9 +15,17 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/[\.\!\?]$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{3}\s\d{3}\s\d{4}|\d{9}|\(\d{3}\)\d{3}\-?\d{4}/)
+    return true
+  else
+    return false
+  end
 end
